59b5a1a4f9a1d8a936998ccdce574c27f37eab6a
Pradyut Sarkar <pradyut.bca15@gmail.com>
2026-04-19 00:10:51 +0530

    Update WebSecurityConfig.java

--- src/main/java/com/intellifleet/config/WebSecurityConfig.java
+++ src/main/java/com/intellifleet/config/WebSecurityConfig.java
@@ -52,6 +52,7 @@ public class WebSecurityConfig {
 						.requestMatchers("/authenticate").permitAll()
 						.requestMatchers("/h2-console/**").permitAll()
 						.requestMatchers("/console/**").permitAll()
+						.requestMatchers("/actuator/**").permitAll()
 						.requestMatchers("/api-docs/**", "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
 						.anyRequest()
 						.authenticated()
